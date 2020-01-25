26caf5fa2c66ba11fa8f663b257a3d87674184f9
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-01-25 02:52:41 -0500

    Variance, Mean, Sum (Base) => in: stats
    Added variance, mean and summation function to the code-base. The summation function will make it awful convenient to not have to reduce to sum up Arrays.

--- lathe.lisp
+++ lathe.lisp
@@ -1,6 +1,20 @@
+;!=== Lathe.lisp Machine-Learning Library ===!
+;|      Lathe Software Foundation 2020       |
+;|          MIT Permissive License           |
+
+; Make Packages
 (make-package :stats)
 (make-package :preprocess)
 (make-package :models)
+; ================ Stats ================__
 (in-package stats)
-(defun mean (x)
+; Summation (Added to avoid reducing so frequently)
+(defun sum  (x) (reduce '+ y)
 )
+; Mean
+(defun mean (x) (/ (sum x) (length x))
+)
+; Variance
+(defun variance (x) (* (/ (sum x) (mean x)) (/ (sum x) (mean x)))
+)
+; Standard Deviation

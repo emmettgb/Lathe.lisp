ff9172d5b94e5bdcd360239e137e044e27229edd
Emmett Boudreau <52672675+emmettgb@users.noreply.github.com>
2020-01-25 19:35:52 -0500

    Added .* => (in-package: stats)
    Defined .* for dot-product single dimensional array multiplication.

--- lathe/lathe.lisp
+++ lathe/lathe.lisp
@@ -6,8 +6,12 @@
 (make-package :stats)
 (make-package :preprocess)
 (make-package :models)
+(make-package :matrix)
 ; ================ Stats ================__
 (in-package stats)
+; Dot (A lot easier)
+(defun .* (x y) * (loop for c in x
+      for z in y (* z c))
 ; Summation (Added to avoid reducing so frequently)
 (defun sum  (x) (reduce '+ y)
 )
@@ -24,3 +28,42 @@
 ; Standard Error
 (defun ste (x) (/ (std x) (length x))
 )
+; Correlation Coefficient (r)
+(defun correlationcoeff (x y) (setq n (length x))
+(setq yn (length y)) (setq Σx (sum x)) (setq Σy (sum y))
+)
+; Correlation Coefficient of Determination (r^2)
+(defun r2 (ŷ y) (setq r (correlationcoeff ŷ y))
+(* r r)
+)
+; ============= Matrix =============__
+(defun gets (x lst)
+    ( if (= x 0) (car lst)
+         (gets (- x 1) (cdr lst) )
+     )
+)
+
+(defun getrow (x matrix)
+    (gets x matrix)
+ )
+
+(defun getcol (x matrix)
+    (if (null matrix) nil
+     (append (list (gets x (car matrix) )) (getcol x (cdr matrix) ))
+        )
+    )
+
+
+(defun helper (line col) (apply #'+ 0 (mapcar #'* line col) ))
+
+(defun helper2 (line matrix x) (if (= -1 x) nil (append
+     (list (helper line (getcol (- (length matrix) x)
+      matrix))) (helper2 line matrix (- x 1))) ))
+
+
+(defun matrixmul (mat1 mat2 x)
+    (if (= x 0) nil (append (list (helper2 (getrow (- (length mat1) x ) mat1)
+     mat2 (length mat2))) (matrixmul mat1 mat2 (- x 1) ) ) )
+)
+
+(defun matmul (mat1 mat2) (matrixmul mat1 mat2 (length mat1) ) )
